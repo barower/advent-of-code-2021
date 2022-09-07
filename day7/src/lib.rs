1a6183a71da8351d3adb16a5450ece0f1b8c4f2f
Bartłomiej Osiński <osinskibartlomiej@gmail.com>
2022-09-08 01:09:00 +0200

    Day 7 part 2

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -13,12 +13,20 @@ impl FromStr for Crabs {
 }
 
 impl Crabs {
-    fn cost_to(&self, position: u64) -> u64 {
+    fn part1_cost_to(&self, position: u64) -> u64 {
         self.0.iter().map(|pos| pos.abs_diff(position)).sum()
     }
 
-    pub fn cheapest_cost(&self) -> u64 {
-        (0..self.0.iter().max().unwrap()+1).map(|pos| self.cost_to(pos)).min().unwrap()
+    fn part2_cost_to(&self, position: u64) -> u64 {
+        self.0.iter().map(|pos| (1..pos.abs_diff(position)+1).sum::<u64>()).sum()
+    }
+
+    pub fn part1_cheapest_cost(&self) -> u64 {
+        (0..self.0.iter().max().unwrap()+1).map(|pos| self.part1_cost_to(pos)).min().unwrap()
+    }
+
+    pub fn part2_cheapest_cost(&self) -> u64 {
+        (0..self.0.iter().max().unwrap()+1).map(|pos| self.part2_cost_to(pos)).min().unwrap()
     }
 }
 
@@ -36,24 +44,42 @@ pub mod tests {
     }
 
     #[test]
-    fn cost_to() -> Result<(), Box<dyn std::error::Error>> {
+    fn part1_cost_to() -> Result<(), Box<dyn std::error::Error>> {
         let input = "16,1,2,0,4,2,7,1,2,14";
         let crabs: Crabs = Crabs::from_str(input)?;
-        assert_eq!(crabs.cost_to(2), 37);
-        assert_eq!(crabs.cost_to(1), 41);
-        assert_eq!(crabs.cost_to(3), 39);
-        assert_eq!(crabs.cost_to(10), 71);
+        assert_eq!(crabs.part1_cost_to(2), 37);
+        assert_eq!(crabs.part1_cost_to(1), 41);
+        assert_eq!(crabs.part1_cost_to(3), 39);
+        assert_eq!(crabs.part1_cost_to(10), 71);
 
         Ok(())
     }
 
     #[test]
-    fn cheapest_cost() -> Result<(), Box<dyn std::error::Error>> {
+    fn part1_cheapest_cost() -> Result<(), Box<dyn std::error::Error>> {
         let input = "16,1,2,0,4,2,7,1,2,14";
         let crabs: Crabs = Crabs::from_str(input)?;
-        assert_eq!(crabs.cheapest_cost(), 37);
+        assert_eq!(crabs.part1_cheapest_cost(), 37);
 
         Ok(())
     }
+
+    #[test]
+    fn part2_cost_to() -> Result<(), Box<dyn std::error::Error>> {
+        let input = "16,1,2,0,4,2,7,1,2,14";
+        let crabs: Crabs = Crabs::from_str(input)?;
+        assert_eq!(crabs.part2_cost_to(5), 168);
+
+        Ok(())
+    }
+    #[test]
+    fn part2_cheapest_cost() -> Result<(), Box<dyn std::error::Error>> {
+        let input = "16,1,2,0,4,2,7,1,2,14";
+        let crabs: Crabs = Crabs::from_str(input)?;
+        assert_eq!(crabs.part2_cheapest_cost(), 168);
+
+        Ok(())
+    }
+
 }
 
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -5,9 +5,12 @@ use std::fs::read_to_string;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
         let input = read_to_string("inputfile")?;
         let crabs: Crabs = Crabs::from_str(input.trim())?;
-        let result = crabs.cheapest_cost();
+        let result = crabs.part1_cheapest_cost();
 
         println!("Minimum fuel cost: {result}");
+
+        let result = crabs.part2_cheapest_cost();
+        println!("Part 2 minimum fuel cost: {result}");
 
         Ok(())
 }
