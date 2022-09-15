89e4f3f84a6ad156be9616ab942fdf5bb06dc6cd
Bartłomiej Osiński <osinskibartlomiej@gmail.com>
2022-09-15 20:24:52 +0200

    Day 9 part 2

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use thiserror::Error;
+use std::collections::HashSet;
 
 #[non_exhaustive]
 #[derive(Error, Debug)]
@@ -13,8 +14,8 @@ pub enum Day8Error {
 pub struct Heatmap(Vec<Vec<u8>>);
 
 impl Heatmap {
-    pub fn find_low_points(&self) -> Vec<u8> {
-        let mut retvec: Vec<u8> = vec![];
+    pub fn find_low_points(&self) -> Vec<(usize, usize)> {
+        let mut retvec = vec![];
 
         for (y, row) in self.0.iter().enumerate() {
             for (x, _) in row.iter().enumerate() {
@@ -22,12 +23,46 @@ impl Heatmap {
                 let y: isize = y.try_into().unwrap();
 
                 if self.is_local_minimum(x, y) {
-                    retvec.push(self.get_at(x, y).unwrap());
+                    retvec.push((x as usize, y as usize));
                 }
             }
         }
 
         retvec
+    }
+
+    pub fn find_all_basins(&self) -> Vec<HashSet<(isize, isize)>> {
+        let mut retvec = vec![];
+
+        for (x, y) in self.find_low_points().iter() {
+            retvec.push(self.find_basin(((*x).try_into().unwrap(), (*y).try_into().unwrap())));
+        }
+
+        retvec
+    }
+
+    fn find_basin(&self, (x, y): (isize, isize)) -> HashSet<(isize, isize)> {
+        let mut basin: HashSet<(isize, isize)> = HashSet::new();
+        let mut candidates: HashSet<(isize, isize)> = vec![(x, y)].into_iter().collect();
+
+        while !candidates.is_empty() {
+            let mut new_candidates: HashSet<(isize, isize)> = HashSet::new();
+
+            for (x, y) in candidates.iter() {
+                if let Some(val) = self.get_at(*x, *y) {
+                    if !basin.contains(&(*x,*y)) && val < 9 {
+                        basin.insert((*x, *y));
+                        for (relx, rely) in [(0, 1), (-1, 0), (1, 0), (0, -1)] {
+                            new_candidates.insert((*x+relx, *y+rely));
+                        }
+                    }
+                }
+            }
+
+            candidates = new_candidates;
+        }
+
+        basin
     }
 
     fn is_local_minimum(&self, x: isize, y: isize) -> bool {
@@ -47,7 +82,7 @@ impl Heatmap {
         true
     }
 
-    fn get_at(&self, x: isize, y: isize) -> Option<u8> {
+    pub fn get_at(&self, x: isize, y: isize) -> Option<u8> {
         if x < 0 || x >= self.width() || y < 0 || y >= self.height() {
             None
         } else {
@@ -86,13 +121,17 @@ impl FromStr for Heatmap {
 mod tests {
     use super::*;
 
-    #[test]
-    fn heatmap_from_str() -> Result<(), Box<dyn std::error::Error>> {
-        let heatmap = Heatmap::from_str("2199943210
+    fn get_test_heatmap() -> Heatmap {
+        Heatmap::from_str("2199943210
 3987894921
 9856789892
 8767896789
-9899965678")?;
+9899965678").unwrap()
+    }
+
+    #[test]
+    fn heatmap_from_str() {
+        let heatmap = get_test_heatmap();
 
         assert_eq!(heatmap.width(), 10);
         assert_eq!(heatmap.height(), 5);
@@ -100,17 +139,11 @@ mod tests {
         assert_eq!(heatmap.get_at(0, 0), Some(2));
         assert_eq!(heatmap.get_at(1, 0), Some(1));
         assert_eq!(heatmap.get_at(0, 1), Some(3));
-
-        Ok(())
     }
 
     #[test]
-    fn heatmap_is_local_minimum() -> Result<(), Box<dyn std::error::Error>> {
-        let heatmap = Heatmap::from_str("2199943210
-3987894921
-9856789892
-8767896789
-9899965678")?;
+    fn heatmap_is_local_minimum() {
+        let heatmap = get_test_heatmap();
 
         assert!(!heatmap.is_local_minimum(0, 0));
         assert!(heatmap.is_local_minimum(1, 0));
@@ -120,34 +153,21 @@ mod tests {
         assert!(!heatmap.is_local_minimum(0, 4));
         assert!(heatmap.is_local_minimum(6, 4));
 
-        Ok(())
     }
 
     #[test]
-    fn heatmap_find_low_points() -> Result<(), Box<dyn std::error::Error>> {
-        let heatmap = Heatmap::from_str("2199943210
-3987894921
-9856789892
-8767896789
-9899965678")?;
-
-        assert_eq!(heatmap.find_low_points(), vec![1, 0, 5, 5]);
-
-        Ok(())
+    fn heatmap_find_low_points() {
+        let heatmap = get_test_heatmap();
+        assert_eq!(heatmap.find_low_points(), vec![(1,0), (9,0), (2,2), (6,4)]);
     }
 
     #[test]
-    fn heatmap_broken_input() -> Result<(), Box<dyn std::error::Error>> {
-        let heatmap = Heatmap::from_str("2199943210
-3987894921
-9856789892
-8767896789
-9899965678
+    fn heatmap_find_basin() {
+        let heatmap = get_test_heatmap();
+        assert_eq!(heatmap.find_basin((0, 0)), vec![(0,0), (1,0), (0,1)].into_iter().collect());
 
-")?;
-
-        assert_eq!(heatmap.find_low_points(), vec![1, 0, 5, 5]);
-
-        Ok(())
+        assert_eq!(heatmap.find_basin((9, 0)).len(), 9);
+        assert_eq!(heatmap.find_basin((0, 3)).len(), 14);
+        assert_eq!(heatmap.find_basin((9, 4)).len(), 9);
     }
 }
--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,8 +6,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_to_string("inputfile")?;
     let heatmap = Heatmap::from_str(&input)?;
 
-    let x: u64 = heatmap.find_low_points().iter().map(|val| (1 + *val) as u64).sum();
+    let x: u64 = heatmap.find_low_points().iter().map(|(x,y)| (1 + heatmap.get_at((*x).try_into().unwrap(), (*y).try_into().unwrap()).unwrap()) as u64).sum();
     println!("Result of part 1 is {}", x);
+
+    let mut p2_results: Vec<_> = heatmap.find_all_basins().into_iter().map(|set| set.len()).collect();
+    p2_results.sort();
+    p2_results = p2_results.into_iter().rev().collect();
+
+    println!("Result of part 2 is {}", p2_results[0] * p2_results[1] * p2_results[2]);
 
     Ok(())
 }
