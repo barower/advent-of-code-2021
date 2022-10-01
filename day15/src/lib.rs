ed30e5548128b435075d9057721ad33f34509aaf
Bartłomiej Osiński <osinskibartlomiej@gmail.com>
2022-10-02 01:33:14 +0200

    Day 15 part 2 (kinda slow, should implement A* instead)

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -15,6 +15,7 @@ pub enum Day15Error {
     UnknownError,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct RiskMap(Vec<Vec<u8>>);
 
 impl RiskMap {
@@ -56,8 +57,64 @@ impl RiskMap {
         self.0.len()
     }
 
+    pub fn expand_right(&mut self, mut times: usize) {
+        let mut new_copy = self.clone();
+        while times > 0 {
+            new_copy.increment();
+            self.append_right(&new_copy);
+            times -= 1;
+        }
+    }
+
+    pub fn expand_down(&mut self, mut times: usize) {
+        let mut new_copy = self.clone();
+        while times > 0 {
+            new_copy.increment();
+            self.append_down(&new_copy);
+            times -= 1;
+        }
+    }
+
     fn get_at(&self, (x, y): (usize, usize)) -> u8 {
         self.0[y][x]
+    }
+
+    fn increment(&mut self) {
+        for val in self.0.iter_mut().flatten() {
+            if *val == 9 {
+                *val = 1;
+            } else {
+                *val += 1;
+            }
+        }
+    }
+
+    fn append_right(&mut self, other: &Self) {
+        let old_width = self.width();
+        let new_width = old_width + other.width();
+
+        for row in self.0.iter_mut() {
+            row.resize(new_width, 0);
+        }
+
+        for (appy, appended_row) in other.0.iter().enumerate() {
+            for (appx, appended_val) in appended_row.iter().enumerate() {
+                self.0[appy][old_width + appx] = *appended_val;
+            }
+        }
+    }
+
+    fn append_down(&mut self, other: &Self) {
+        let old_height = self.height();
+        let new_height = old_height + other.height();
+
+        self.0.resize(new_height, vec![0; self.width()]);
+
+        for (appy, appended_row) in other.0.iter().enumerate() {
+            for (appx, appended_val) in appended_row.iter().enumerate() {
+                self.0[old_height + appy][appx] = *appended_val;
+            }
+        }
     }
 }
 
@@ -124,4 +181,178 @@ mod tests {
 
         assert_eq!(riskmap.dijkstra_lowest_risk_path((0, 0), (9, 9)), Weight::NonInf(40));
     }
+
+    #[test]
+    fn riskmap_increment() {
+        let mut riskmap = get_test_riskmap();
+        riskmap.increment();
+
+        let expected = RiskMap::from_str("2274862853
+2492484783
+3247622439
+4715142671
+8574528222
+2421239248
+2461123532
+4236532741
+2314249632
+3422155692").unwrap();
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_append_right() {
+        let mut riskmap = get_test_riskmap();
+
+        let to_append = RiskMap::from_str("2274862853
+2492484783
+3247622439
+4715142671
+8574528222
+2421239248
+2461123532
+4236532741
+2314249632
+3422155692").unwrap();
+
+        riskmap.append_right(&to_append);
+
+        let expected = RiskMap::from_str("11637517422274862853
+13813736722492484783
+21365113283247622439
+36949315694715142671
+74634171118574528222
+13191281372421239248
+13599124212461123532
+31254216394236532741
+12931385212314249632
+23119445813422155692").unwrap();
+
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_expand_right_once() {
+        let mut riskmap = get_test_riskmap();
+
+        riskmap.expand_right(1);
+
+        let expected = RiskMap::from_str("11637517422274862853
+13813736722492484783
+21365113283247622439
+36949315694715142671
+74634171118574528222
+13191281372421239248
+13599124212461123532
+31254216394236532741
+12931385212314249632
+23119445813422155692").unwrap();
+
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_expand_right_twice() {
+        let mut riskmap = get_test_riskmap();
+
+        riskmap.expand_right(2);
+
+        let expected = RiskMap::from_str("116375174222748628533385973964
+138137367224924847833513595894
+213651132832476224394358733541
+369493156947151426715826253782
+746341711185745282229685639333
+131912813724212392483532341359
+135991242124611235323572234643
+312542163942365327415347643852
+129313852123142496323425351743
+231194458134221556924533266713").unwrap();
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_append_down() {
+        let mut riskmap = get_test_riskmap();
+
+        let to_append = RiskMap::from_str("2274862853
+2492484783
+3247622439
+4715142671
+8574528222
+2421239248
+2461123532
+4236532741
+2314249632
+3422155692").unwrap();
+
+        riskmap.append_down(&to_append);
+
+        let expected = RiskMap::from_str("1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+2274862853
+2492484783
+3247622439
+4715142671
+8574528222
+2421239248
+2461123532
+4236532741
+2314249632
+3422155692").unwrap();
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_expand_down_once() {
+        let mut riskmap = get_test_riskmap();
+
+        riskmap.expand_down(1);
+
+        let expected = RiskMap::from_str("1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+2274862853
+2492484783
+3247622439
+4715142671
+8574528222
+2421239248
+2461123532
+4236532741
+2314249632
+3422155692").unwrap();
+
+        assert_eq!(riskmap, expected);
+    }
+
+    #[test]
+    fn riskmap_dijkstra_expanded() {
+        let mut riskmap = get_test_riskmap();
+
+        riskmap.expand_right(4);
+        riskmap.expand_down(4);
+
+        assert_eq!(riskmap.dijkstra_lowest_risk_path((0, 0), (49, 49)), Weight::NonInf(315));
+    }
+
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -4,10 +4,17 @@ use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_to_string("inputfile")?;
-    let riskmap: RiskMap = RiskMap::from_str(input.trim())?;
-    let destination = (riskmap.width()-1, riskmap.height()-1);
+    let mut riskmap: RiskMap = RiskMap::from_str(input.trim())?;
+    let mut destination = (riskmap.width()-1, riskmap.height()-1);
 
     println!("Part 1's result is {:?}", riskmap.dijkstra_lowest_risk_path((0, 0), destination));
+
+    riskmap.expand_right(4);
+    riskmap.expand_down(4);
+
+    destination = (riskmap.width()-1, riskmap.height()-1);
+
+    println!("Part 2's result is {:?}", riskmap.dijkstra_lowest_risk_path((0, 0), destination));
 
     Ok(())
 }
